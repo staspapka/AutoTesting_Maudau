224384d5b4cd1e4c239f10678b6ec6e95b1b9ec9
Stas-Git <stanislavpapka@gmail.com>
2026-02-19 15:00:49 +0200

    Added Faker foe API tests data

--- testData/api/deviceData.ts
+++ testData/api/deviceData.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 export const devicePayloads = {
   iphone14: {
     name: 'Apple iPhone 14 Pro',
@@ -11,14 +13,13 @@ export const devicePayloads = {
   iphone14Updated: {
     name: 'Apple iPhone 14 Pro (Updated)',
     data: {
-      year: 2023,
-      price: 1099.99,
-      'CPU model': 'A16 pro',
-      'Hard disk size': '1 TB',
+      year: faker.number.int({ min: 2020, max: 2025 }),
+      price: parseFloat(faker.commerce.price({ min: 800, max: 1500 })),
+      'CPU model': 'A16',
+      'Hard disk size': `${faker.helpers.arrayElement(['128', '256', '512'])} GB`,
     },
   },
   iphonePatch: {
     name: 'iPhone 14 Pro Deep Purple',
   },
 };
-// USE FAKER TO GENERATE DATA
